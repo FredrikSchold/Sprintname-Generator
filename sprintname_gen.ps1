d6d70a07d7106421fb1ab803c3457fb43f9fd13e
Fredrik Schöld <71184994+FredrikSchold@users.noreply.github.com>
2022-01-03 10:58:47 +0100

    Add files via upload

---
+++ sprintname_gen.ps1
@@ -0,0 +1,16 @@
+﻿$candy = Get-Content ".\Powershell\sprintname\candy.txt"
+$spices = Get-Content ".\Powershell\sprintname\spices.txt"
+
+function Get-sprintname {
+$spices = $spices -split ","
+write-host ""
+$candy = $candy -split ","
+
+$spices = Get-Random $spices
+$candy = Get-Random $candy 
+Write-host $spices$candy
+}
+
+foreach ($i in 1..10){
+Get-sprintname
+}
